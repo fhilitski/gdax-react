976a7fbf9fb7763b00165e317c3410ba2a97b8f7
f.hilitski@gmail.com <f.hilitski@gmail.com>
2018-04-19 23:01:09 -0400

    added header component for the App added bootstrap css styles to package.json

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">GDAX Ticker</h1>
-        </header>
+        <Header/>
         <p className="App-intro">
           -abc-
         </p>
