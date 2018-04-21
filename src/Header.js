353c606630948f3744ddbfd8359c0e7093b3e376
f.hilitski@gmail.com <f.hilitski@gmail.com>
2018-04-20 21:20:42 -0400

    adding a header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Header = (props) => {
+    return (
+        <header className="jumbotron">
+          <h1>GDAX Ticker</h1>
+	  <p>Tracking cryptocurency exchange rates</p>
+        </header>
+    );
+  };
+
+export default Header;
